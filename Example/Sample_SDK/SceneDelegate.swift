0e0fb50480c2e2c5cc35a56982eec2843c6255f4
chrisvin-p-8413 <chrisvin.p@zohocorp.com>
2026-09-07 17:24:58 +0530

    Add instantiate call in SceneDelegate

--- Example/Sample_SDK/SceneDelegate.swift
+++ Example/Sample_SDK/SceneDelegate.swift
@@ -4,12 +4,14 @@
 //
 
 import UIKit
+import Lens_Integration_SDK
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        LensSDK.shared.instantiate()
         guard scene is UIWindowScene else { return }
     }
 }
